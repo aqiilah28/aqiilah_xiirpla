9594a1d6248d635a103a3f93b3e2d2445270c7a1
aqiilah28 <64079686+aqiilah28@users.noreply.github.com>
2020-09-08 13:27:05 +0700

    Add files via upload

---
+++ uts1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts;
+
+/**
+ *
+ * @author aqilah
+ */
+public class uts1 { 
+    public static void main(String[] args) {
+       String str="ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+        System.out.println(""+ str.charAt(0) + str.charAt(16) + str.charAt(8) + str.charAt(8) + str.charAt(11) + str.charAt(0) + str.charAt(7) + str.charAt(27) + str.charAt(22)+str.charAt(8)+ str.charAt(3)+str.charAt(24)+ str.charAt(0));
+     
+    }
+    
+}
